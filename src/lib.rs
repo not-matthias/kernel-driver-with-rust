b252bfb3ec8a4e188b39e0da3158abbf13daf464
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-10-16 21:43:28 +0200

    feat: add `kernel-build` and `kernel-log` crates

--- src/lib.rs
+++ src/lib.rs
@@ -4,51 +4,34 @@
 extern crate alloc;
 
 use crate::{include::MmIsAddressValid, process::Process, string::create_unicode_string};
-use core::panic::PanicInfo;
+use kernel_log::KernelLogger;
+use log::LevelFilter;
 
 pub mod include;
-pub mod log;
 pub mod process;
 pub mod string;
-
-/// When using the alloc crate it seems like it does some unwinding. Adding this
-/// export satisfies the compiler but may introduce undefined behaviour when a
-/// panic occurs.
-#[no_mangle]
-pub extern "system" fn __CxxFrameHandler3(_: *mut u8, _: *mut u8, _: *mut u8, _: *mut u8) -> i32 { unimplemented!() }
 
 #[global_allocator]
 static GLOBAL: kernel_alloc::KernelAlloc = kernel_alloc::KernelAlloc;
 
-/// Explanation can be found here: https://github.com/Trantect/win_driver_example/issues/4
-#[export_name = "_fltused"]
-static _FLTUSED: i32 = 0;
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! { loop {} }
-
 #[no_mangle]
 pub extern "system" fn driver_entry() -> u32 {
+    KernelLogger::init(LevelFilter::Info).unwrap();
+
     // MmIsAddressValid
     //
     let is_valid = unsafe { MmIsAddressValid(0 as _) };
-    log!("MmIsAddressValid(0) returned %i", is_valid as u64);
+    log::info!("MmIsAddressValid(0) returned {}", is_valid as u64);
 
     // String
     //
     let string = create_unicode_string(obfstr::wide!("Hello World!\0"));
-    log!("String: %ws", string.Buffer);
+    log::info("String buffer is at: {:x}", string.Buffer);
 
     // Process
     //
     let process = Process::by_id(4 as _);
-    log!("Process found: %i", process.is_some() as u64);
-
-    // kernel-print
-    //
-    kernel_print::kernel_dbg!(2 + 2);
-    kernel_print::kernel_print!("{} + {} = {}\n", 2, 2, 2 + 2);
-    kernel_print::kernel_println!("{} + {} = {}", 2, 2, 2 + 2);
+    log::info!("Process found: {}", process.is_some() as u64);
 
     0 /* STATUS_SUCCESS */
 }
